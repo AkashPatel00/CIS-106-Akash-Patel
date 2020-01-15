90eb2c44b3a8769c69554dc78a24c61de7be4a43
AkashPatel00 <59898098+AkashPatel00@users.noreply.github.com>
2020-01-14 21:34:17 -0600

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+def sandwich():
+    print("Take two slices of bread")
+    print("Get peanut butter")
+    print("Open peanut butter")
+    print("Get a knife")
+    print("Put knife in peanut butter")
+    print("Spread peanut butter on one slice of bread")
+    print("Get jar of Jam")
+    print("Open Jam jar")
+    print("Put knife in jam")
+    print("Spread jam on other slice of bread")
+    print("Combine both slices")
+    print("Enjoy!")
+
+# Main

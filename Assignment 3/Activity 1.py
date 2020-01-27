3181d1c7bf95465d615d12f355d4a330d51905a5
AkashPatel00 <59898098+AkashPatel00@users.noreply.github.com>
2020-01-27 12:36:55 -0600

    Add files via upload

---
+++ Assignment 3/Activity 1.py
@@ -0,0 +1,10 @@
+print("Enter hours worked in a week")
+hours = int(input())
+print("What is your pay rate?")
+rate = int(input())
+pay = hours * rate
+monthly = hours * rate * 3
+yearly = hours * rate * 52
+print("Your Weekly pay is.." + str(pay))
+print("Your Monthly pay is.." + str(monthly))
+print("Your Yearly pay is.." + str(yearly))

a250e0c9ad5517b1b98fe41688c81a3ea56a49e2
AkashPatel00 <59898098+AkashPatel00@users.noreply.github.com>
2020-01-27 12:35:44 -0600

    Add files via upload

---
+++ Assignment 3/Activity 2.py
@@ -0,0 +1,12 @@
+print("How old are you?")
+age = int(input())
+years = age * 1
+months = age * 12
+days = age * 365
+hours = age * 8760
+seconds = age * 31536000
+print("You are.." + str(years) + "years old")
+print("You are" + str(months) + "months old")
+print("You are" + str(days) + "days old")
+print("You are" + str(hours) + "hours old")
+print("You are" + str(seconds) + "seconds old")

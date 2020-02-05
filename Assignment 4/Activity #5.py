05ab1bb1e496abead9f0f16fcdc4d2270ac6a190
AkashPatel00 <59898098+AkashPatel00@users.noreply.github.com>
2020-02-05 13:04:48 -0600

    Add files via upload

---
+++ Assignment 4/Activity #5.py
@@ -0,0 +1,6 @@
+# This program's purpose is to determine how many square yards are in a specific surface
+
+Width = float(input("What is the width of the room?"))
+Length = float(input("What is the length of the room?"))
+Area= (Length * Width)
+print("The amount of floor covering the surface is.." + str(Area/9))

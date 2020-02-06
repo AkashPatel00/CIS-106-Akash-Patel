25e8bafe0379362591ce0f35c915f59589ed009e
Dave Braunschweig <dbraunsc@harpercollege.edu>
2020-02-06 10:48:17 -0600

    Assignment 4 corrections

--- Assignment 4/Activity #3.py
+++ Assignment 4/Activity #3.py
@@ -1,10 +1,13 @@
-# The purpose of this program is to be able to convert miles to yards, feet and inches.
+# The purpose of this program is to be able to convert miles to yards, 
+# feet and inches.
 
 print("Enter amount of miles")
 miles = int(input())
+
 yards = miles * 1760
 feet = miles * 5280
 inches= miles * 63360
+
 print("Your distance in yards is.." + str(yards))
 print("Your distance in feet is.." + str(feet))
 print("Your distance in inches is.." + str(inches))

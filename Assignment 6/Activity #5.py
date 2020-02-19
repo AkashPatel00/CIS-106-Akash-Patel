95bd30ab302f66a6aab4a1ce1d76341411738af6
AkashPatel00 <59898098+AkashPatel00@users.noreply.github.com>
2020-02-19 08:26:00 -0600

    Add files via upload

--- Assignment 6/Activity #5.py
+++ Assignment 6/Activity #5.py
@@ -1,29 +1,29 @@
 # This program's purpose is to determine how many square yards are in a specific surface.
 
-def calculateArea(length, width):
+def calculate_area(length, width):
     area = length * width
 
     return area
 
 
-def calculateSurface(area):
+def calculate_surface(area):
     surface = area / 9
 
     return surface
 
 
-def displayResult(surface):
+def display_result(surface):
     print("The amount of floor covering the surface is.." + str(surface))
 
 
-def getLength():
+def get_length():
     print("What is the length of the room?")
     length = float(input())
 
     return length
 
 
-def getWidth():
+def get_width():
     print("What is the width of the room?")
     width = float(input())
 
@@ -31,9 +31,9 @@ def getWidth():
 
 
 # Main
-length = getLength()
-width = getWidth()
-area = calculateArea(length, width)
-area = calculateSurface(area)
-surface = calculateSurface(area)
-displayResult(surface)
+length = get_length()
+width = get_width()
+area = calculate_area(length, width)
+area = calculate_surface(area)
+surface = calculate_surface(area)
+display_result(surface)
--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -2,52 +2,52 @@
 # it then calculates their weekly, monthly and annual income
 
 
-def calculatePay(hours, rate):
+def calculate_pay(hours, rate):
     pay = rate * hours
 
     return pay
 
 
-def calculateWeekly(pay):
-    weekly = pay * 7
+def calculate_weekly(pay):
+    weekly = pay
 
     return weekly
 
 
-def calculateMonthly(pay):
+def calculate_monthly(pay):
     monthly = pay * 30
 
     return monthly
 
 
-def calculateAnnual(pay):
-    annual = pay * 365
+def calculate_annual(pay):
+    annual = pay * 52
 
     return annual
 
 
-def displayresult(pay, weekly, monthly, annual):
+def display_result(pay, weekly, monthly, annual):
     print("Your weekly pay is.." + str(weekly) + "Your monthly pay is" + str(monthly) + "Your annual pay is" + str(annual))
 
 
-def getRate():
+def get_rate():
     print("Enter pay rate")
     rate = float(input())
 
     return rate
 
 
-def getHours():
+def get_hours():
     print("Enter hours worked")
     hours = float(input())
 
     return hours
 
 # Main
-rate = getRate()
-hours = getHours()
-pay = calculatePay(hours, rate)
-weekly = calculateWeekly(pay)
-monthly = calculateMonthly(pay)
-annual = calculateAnnual(pay)
-displayresult(pay, weekly, monthly, annual)
+rate = get_rate()
+hours = get_hours()
+pay = calculate_pay(hours, rate)
+weekly = calculate_weekly(pay)
+monthly = calculate_monthly(pay)
+annual = calculate_annual(pay)
+display_result(pay, weekly, monthly, annual)

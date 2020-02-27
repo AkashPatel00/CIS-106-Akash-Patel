0f2cd0984f882d5a5ca0432d39442c92e14e6818
AkashPatel00 <59898098+AkashPatel00@users.noreply.github.com>
2020-02-27 09:55:49 -0600

    Add files via upload

--- Assignment 7/activity 3.py
+++ Assignment 7/activity 3.py
@@ -1,17 +1,4 @@
-def get_choice():
-    print("Enter U if you want to convert into US or M if you want to convert into Metric:")
-    choice = input()
-    return choice
-
-
-def process_US():
-    miles = get_miles("US")
-    calculate_us(miles)
-
-
-def process_Metric():
-    miles = get_miles("Metric")
-    calculate_metric(miles)
+# Program asks user for amount in miles then asks to convert into US measurements or Metric then converts
 
 
 def get_miles(scale):
@@ -19,6 +6,21 @@ def get_miles(scale):
     miles = float(input())
     return miles
 
+
+def get_choice():
+    print("Enter U if you want to convert into US or M if you want to convert into Metric:")
+    choice = input()
+    return choice
+
+
+def process_us():
+    miles = get_miles("US")
+    calculate_us(miles)
+
+
+def process_metric():
+    miles = get_miles("Metric")
+    calculate_metric(miles)
 
 def calculate_us(miles):
     yards = (miles * 1760)
@@ -31,13 +33,25 @@ def calculate_metric(miles):
     kilometers = (miles * 1.60934)
     meters = (miles * 1609.34)
     centimeters = (miles * 160934)
-    print (str(miles) + "miles," + str(kilometers) + "kilometers," + str(meters) + "meters," + str(centimeters) + "centimeters.")
+    print(str(miles) + "miles," + str(kilometers) + "kilometers," + str(meters) + "meters," + str(centimeters) + "centimeters.")
 
 
 choice = get_choice()
 if choice == "U" or choice == "u":
-    process_US()
+    process_us()
 elif choice == "M" or choice == "m":
-    process_Metric()
+    process_metric()
 else:
     print("You must insert U or M to convert your miles")
+
+#Main
+miles = get_miles()
+choice = get_choice()
+yards = calculate_us(miles)
+feet = calculate_us(miles)
+inches = calculate_us(miles)
+kilometers = calculate_metric(miles)
+meters = calculate_metric(miles)
+
+
+
